c5e993b23fd04c818ae1013bb305d3624346d403
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-08-31 12:46:30 +0500

    Added roles in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :timeoutable
+
+  enum role: [:admin, :agent, :branch_officer, :consellor]
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :admin
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_160642) do
+ActiveRecord::Schema.define(version: 2018_08_31_074030) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_08_30_160642) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
