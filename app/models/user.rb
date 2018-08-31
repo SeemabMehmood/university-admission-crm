f5245d118b64b028a33bda32515df85fd9b017b0
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-08-31 13:17:21 +0500

    Code Refactor & Added Status enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :timeoutable
 
-  enum role: [:admin, :agent, :branch_officer, :consellor]
-
-  after_initialize :set_default_role, if: :new_record?
-
-  def set_default_role
-    self.role ||= :admin
-  end
+  enum status: [:active, :inactive]
+  enum role:   [:admin, :agent, :branch_officer, :consellor]
 end
