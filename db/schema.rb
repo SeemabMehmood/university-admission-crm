0d3774ecda01e85de5ee4caee52ff41a8f0e1bc3
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-01 20:47:09 +0500

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_151110) do
+ActiveRecord::Schema.define(version: 2018_09_01_132139) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 2018_08_31_151110) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 0
     t.string "state"
+    t.string "type"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
