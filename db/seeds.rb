85cda16bab33c72633970f13422549121a8b131c
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-02 17:00:13 +0500

    Updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,2 @@
-User.create! do |u|
-  u.email    = 'admin@aed-crm.com'
-  u.password = 'pass123!'
-
-  u.country = "USA"
-  u.name = "Access Education Admin"
-  u.confirmed_at = DateTime.now
-end
+admin = User.first_or_initialize(email: "admin@aed-crm.com", password: "pass123!", password_confirmation: "pass123!", country: "USA", name: "Access Education Admin", confirmed_at: DateTime.now)
+admin.save!
