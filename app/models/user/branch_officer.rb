8e33089539c652e4f7e6e73abb78ef9e4a73ef55
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-02 18:07:19 +0500

    fixed STI error

--- app/models/user/branch_officer.rb
+++ app/models/user/branch_officer.rb
@@ -1,7 +1,7 @@
 class BranchOfficer < User
   has_many :counsellors, foreign_key: :branch_officer_id
 
-  belongs_to :agent, foreign_key: :agent_id
+  belongs_to :agent
 
   def subordinate_count
     [counsellors.count, " Counsellors"].join
--- app/models/user/counsellor.rb
+++ app/models/user/counsellor.rb
@@ -1,5 +1,5 @@
 class Counsellor < User
-  belongs_to :branch_officer, foreign_key: :branch_officer_id
+  belongs_to :branch_officer
 
   def subordinate_count
     "Applications count"
