64b48e76edcf5c6c30e6e28394d9f5b856a04e2f
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-03 19:04:21 +0500

    Fixed indentation branch officer model

--- app/models/user/branch_officer.rb
+++ app/models/user/branch_officer.rb
@@ -2,13 +2,13 @@ class BranchOfficer < User
   has_many :counsellors, foreign_key: :branch_officer_id
 
   filterrific(
-     default_filter_params: { sorted_by: 'created_at_desc' },
-     available_filters: [
-       :sorted_by,
-       :with_role,
-       :with_country_name
-     ]
-   )
+    default_filter_params: { sorted_by: 'created_at_desc' },
+    available_filters: [
+     :sorted_by,
+     :with_role,
+     :with_country_name
+    ]
+ )
 
   belongs_to :agent
 
