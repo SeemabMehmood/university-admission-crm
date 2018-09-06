a89e4d36706ce5d44e433748c88c59672071e9f0
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-06 20:45:44 +0500

    Fixed issue for counsellor show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -21,7 +21,7 @@ module UsersHelper
   def show_superior_name(user)
     return if user.agent? || user.admin?
     sup_name =  ["| Corresponding Agent: ", link_to(user.agent.name.titleize, user_path(user.agent_id))].join() if user.branch_officer?
-    sup_name = ["| Corresponding Branch Officer: ", link_to(user.branch_officer.branch_officer.name.titleize, user_path(user.branch_officer_id))].join() if user.counsellor?
+    sup_name = ["| Corresponding Branch Officer: ", link_to(user.branch_officer.name.titleize, user_path(user.branch_officer_id))].join() if user.counsellor?
     sup_name.html_safe
   end
 end
