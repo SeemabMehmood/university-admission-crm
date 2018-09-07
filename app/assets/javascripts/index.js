f719bac3a1db295b17b48d2af5a075484bbb55ce
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-07 17:07:59 +0500

    Removed extra

--- app/assets/javascripts/index.js
+++
@@ -1,9 +0,0 @@
-// Index Page Js File From Vendor
-//= require jquery.flot
-//= require jquery.flot.resize
-//= require flot.curvedlines/curvedLines
-//= require jquery.flot.spline/index
-//= require icheck
-//= require jquery.peity
-//= require Chart.min
-//= require charts
