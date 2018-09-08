d76c0f2b80ae1393967cacbb23bf727a0ca23414
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-08 10:08:05 +0500

    Added default logo file

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,4 +12,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   def filename
     File.basename(path) if path.present?
   end
+
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  end
 end
