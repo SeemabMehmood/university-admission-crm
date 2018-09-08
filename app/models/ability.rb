91d793f291b345ac0d74a4a8c681650519e6cb40
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-08 13:31:38 +0500

    Added permissions for application process model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,11 +8,13 @@ class Ability
         can :manage, :all
 
       elsif user.agent?
+        can :manage, ApplicationProcess
         can :manage, RepresentingCountry
         can :manage, User, agent_id: user.id
 
         can [:read, :edit, :update], User, id: user.id
       elsif user.branch_officer?
+        can :manage, ApplicationProcess
         can [:edit, :read, :update], RepresentingCountry
         can :manage, User, branch_officer_id: user.id
 
