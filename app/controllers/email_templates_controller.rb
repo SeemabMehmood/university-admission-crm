227b1bc0a685134b0412b958673c982ab2379512
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-08 18:37:49 +0500

    Email template update added

--- app/controllers/email_templates_controller.rb
+++ app/controllers/email_templates_controller.rb
@@ -13,20 +13,27 @@ class EmailTemplatesController < ApplicationController
   end
 
   def create
-    @email_template = EmailTemplate.new(email_template_params.except(:representing_country_id))
+    @email_template = EmailTemplate.new(email_template_params)
 
     respond_to do |format|
       if @email_template.save
         format.html { redirect_to representing_country_path(@email_template.application_process.representing_country.id), notice: 'Email Template was successfully created.' }
-        format.json { render :show, status: :created, location: @email_template }
       else
-        format.html { render :new }
-        format.json { render json: @email_template.errors, status: :unprocessable_entity }
+        format.html { render "email_templates/new" }
+        format.js { render "email_templates/new.js.erb" }
       end
     end
   end
 
-  def edit
+  def update
+    respond_to do |format|
+      if @email_template.update(email_template_params)
+        format.html { redirect_to representing_country_path(@email_template.application_process.representing_country.id), notice: 'Email Template was successfully updated.' }
+      else
+        format.html
+        format.js { render "email_templates/edit.js.erb" }
+      end
+    end
   end
 
   private
