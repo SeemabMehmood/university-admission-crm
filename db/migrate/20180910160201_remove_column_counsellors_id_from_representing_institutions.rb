23215296f41bba81dbb551657375feaccfb1a129
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-10 21:41:16 +0500

    Column name updated in the wrong migration

--- db/migrate/20180910160201_remove_column_counsellors_id_from_representing_institutions.rb
+++ db/migrate/20180910160201_remove_column_counsellors_id_from_representing_institutions.rb
@@ -1,5 +1,5 @@
 class RemoveColumnCounsellorsIdFromRepresentingInstitutions < ActiveRecord::Migration[5.2]
   def change
-    remove_column :representing_institutions, :counsellors_id, :integer, index: true, foreign_key: true
+    add_column :representing_institutions, :counsellor_id, :integer, index: true, foreign_key: true
   end
 end
