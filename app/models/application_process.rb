d7debf49b9fd5736d7ef646348393dbc35c77d39
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-10 21:26:36 +0500

    Code refactor

--- app/models/application_process.rb
+++ app/models/application_process.rb
@@ -5,8 +5,17 @@ class ApplicationProcess < ApplicationRecord
 
   has_one :email_template
 
-  validates :name, presence: true
-  validates :serial, uniqueness: true, presence: true
+  validates :name, :serial, presence: true
 
   default_scope { order(serial: :asc) }
+
+  before_save :check_if_serial_unique
+
+  private
+
+    def check_if_serial_unique
+      if representing_country.application_processes.pluck(:serial).include?(self.serial)
+        errors.add(:base, "cannot have duplicate serial nunbers.")
+      end
+    end
 end
