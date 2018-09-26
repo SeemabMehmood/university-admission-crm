7442590cc51e02758ebd86882429b4d3705107aa
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-26 21:47:18 +0500

    Updated reset error message

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,7 +2,7 @@ class PasswordsController < Devise::PasswordsController
   def update
     super do |resource|
       if resource.errors.any?
-        flash[:notice] = resource.errors.full_messages.to_sentence
+        flash[:notice] = resource.errors.full_messages.map { |msg| msg == 'Reset password token is invalid' ? 'The Reset Password Token is expired. Please send instructions again.' : msg }.to_sentence
       end
     end
   end
